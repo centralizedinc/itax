9695325e8ccb9829746a08c7f01d5943d74c3431
VPBelo <venus.belo.rhi@gmail.com>
2019-10-14 15:50:51 +0800

    form1603Model

--- api/models/forms/form1603Model.js
+++ api/models/forms/form1603Model.js
@@ -20,30 +20,47 @@ var Form1603Schema = new Schema({
     taxable_amount: { type: Number, default: 0 },
     compute_monetary_value: { type: Number, default: 0 },
     compute_tax_due: { type: Number, default: 0 },
-    percentDivStr: Number,
-    amtDueCrdtb: Number,
-    prevTaxPidCrdtb: Number,
-    amtPayblCrdtb: Number,
-    surcharge: Number,
-    interest: Number,
-    compromise: Number,
-    penaltiesCrdtb: Number,
-    totalAmtPayblCrdtb: Number,
-    refundType: String,
-    advPayment: Number,
-    totTaxCredits: Number,
-    ifOverremittance: String,
-    // atcList: [{
-    //     seqNo: Number,
-    //     atcCode: String,
-    //     payeeFlag: String,
-    //     txbleAmt: Number,
-    //     taxRate: Number,
-    //     taxDue: Number,
-    //     description: String
-    // }],
-    dateFiled: Date,
-    dueDate: Date
+    percentage_divisor: { type: Number, default: 0 },
+    tax_due: { type: Number, default: 0 },
+    total_payments_made: { type: Number, default: 0 },
+    other_payments_made: { type: Number, default: 0 },
+    surcharge: { type: Number, default: 0 },
+    interest: { type: Number, default: 0 },
+    compromise: { type: Number, default: 0 },
+    penalties: { type: Number, default: 0 },
+    total_amount_payable: Number,
+    refund_type: String,
+    adv_payment: { type: Number, default: 0 },
+    total_tax_credits: { type: Number, default: 0 },
+    if_overremittance: String,
+    atcList: [{
+        seq_no: { type: Number, default: 0 },
+        atc_code: String,
+        payee_flag: String,
+        taxable_amount: { type: Number, default: 0 },
+        tax_rate: { type: Number, default: 0 },
+        tax_due: { type: Number, default: 0 },
+        description: String
+    }],
+    date_filed: Date,
+    due_date: Date,
+    date_created: {
+        type: Date,
+        default: new Date()
+    },
+    date_modified: {
+        type: Date,
+        default: new Date()
+    },
+    auto_id: {
+        type: Number
+    },
+    created_by: {
+            type: String
+    },
+    modified_by: {
+            type: String
+    }
 });
 
 
@@ -61,27 +78,5 @@ Form1603Schema.pre('findOneAndUpdate', function (callback) {
     this._update.date_modified = new Date();
     callback();
 });
-
-const options = {
-    field: 'auto_id', // auto_id will have an auto-incrementing value
-    incrementBy: 1, // incremented by 2 every time
-    nextCount: false, // Not interested in getting the next count - don't add it to the model
-    // resetCount: 'reset', // The model and each document can now reset the counter via the reset() method
-    startAt: 0, // Start the counter at 1000
-    unique: false // Don't add a unique index
-};
-
-const plugin = new autoIncrement(Form1601CSchema, '1603_forms', options);
-// users._nextCount()
-//     .then(count => console.log(`The next ID will be ${count}`));
-plugin.applyPlugin()
-    .then(() => {
-        console.log("############### init plugin")
-    })
-    .catch(e => {
-        // Plugin failed to initialise
-        console.log("############### init failed: " + e);
-    });
-
 
 module.exports = mongoose.model('1603_forms', Form1603Schema);
