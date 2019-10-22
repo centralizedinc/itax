61457afaf3c6df12fcbfebd5b91f137b0107cded
VPBelo <venus.belo.rhi@gmail.com>
2019-10-22 10:30:33 +0800

    form1601CModel

--- api/models/forms/form1601CModel.js
+++ api/models/forms/form1601CModel.js
@@ -15,22 +15,89 @@ const model_schema = {
     atc_code: {
         type: String
     }, // WW010
-    compensation: { type: Number, default: 0 }, //item14
-    statutory_minimum_wage: { type: Number, default: 0 }, //item15
-    minimum_wager: { type: Number, default: 0 },
-    non_taxable_compensation: { type: Number, default: 0 }, //Sum of Items 15 to 20
-    taxable_compensation: { type: Number, default: 0 }, //Item 14 Less Item 21
-    tax_required: { type: Number, default: 0 },
-    adjustment: { type: Number, default: 0 }, //From Part IV-Schedule 1, Item 4
-    tax_required_remittance: { type: Number, default: 0 }, //Sum of Items 25 and 26
-    tax_remitted: { type: Number, default: 0 }, //Sum of Items 28 and 29
-    other_payments_made: { type: Number, default: 0 },
-    total_payments_made: { type: Number, default: 0 },
+    compensation: {
+        type: Number,
+        default: 0
+    }, //item14
+    statutory_minimum_wage: {
+        type: Number,
+        default: 0
+    }, //item15
+    minimum_wager: {
+        type: Number,
+        default: 0
+    },
+    hazard_pay: {
+        type: Number,
+        default: 0
+    }, //item16 for MWEs only
+    other_benefits: {
+        type: Number,
+        default: 0
+    }, //item17 
+    minimis_benefits: {
+        type: Number,
+        default: 0
+    }, //item18
+    mandatory_contribution: {
+        type: Number,
+        default: 0
+    }, //item19
+    other_taxable_compensation: {
+        type: Number,
+        default: 0
+    }, //item20
+    non_taxable_compensation: {
+        type: Number,
+        default: 0
+    }, //item21 Sum of Items 15 to 20
+    total_taxable_compensation: {
+        type: Number,
+        default: 0
+    }, //item22 Item 14 Less Item 21
+    less_taxable_compensation: {
+        type: Number,
+        default: 0
+    }, //item23 for employees, other than MWEs, recieving 250,000 & below for the year
+    net_taxable_required: {
+        type: Number,
+        default: 0
+    }, //item24 item 22 less item 23
+    total_taxes_withheld: {
+        type: Number,
+        default: 0
+    }, //item25
+    adjustment: {
+        type: Number,
+        default: 0
+    }, //item26 From Part IV-Schedule 1, Item 4
+    tax_required_remittance: {
+        type: Number,
+        default: 0
+    }, //item27 Sum of Items 25 and 26
+    tax_remitted_previously: {
+        type: Number,
+        default: 0
+    }, //item28 
+    other_payments_made: {
+        type: Number,
+        default: 0
+    }, //item29
+    total_payments_made: {
+        type: Number,
+        default: 0
+    }, //item30 Sum of Items 28 and 29
     refund_type: {
         type: String
     },
-    adv_payment: { type: Number, default: 0 },
-    total_tax_credits: { type: Number, default: 0 },
+    adv_payment: {
+        type: Number,
+        default: 0
+    },
+    total_tax_credits: {
+        type: Number,
+        default: 0
+    },
     particular_cash: [{
         drawee_bank: {
             type: Number,
@@ -45,7 +112,6 @@ const model_schema = {
         },
         amount: {
             type: Number,
-            default: 0
         }
     }],
     particular_check: [{
@@ -99,6 +165,7 @@ const model_schema = {
             default: 0
         }
     }],
+    //Schedule 1 Adjustment of Taxes Withheld
     sched1: [{
         previous_month: {
             type: Date,
@@ -127,7 +194,10 @@ const model_schema = {
     }],
 };
 
-var Form1601CSchema = new Schema({...common_model, ...model_schema });
+var Form1601CSchema = new Schema({
+    ...common_model,
+    ...model_schema
+});
 
 Form1601CSchema.pre('save', function(callback) {
     var form = this;
@@ -158,9 +228,9 @@ const plugin = new autoIncrement(Form1601CSchema, '1601c_forms', options);
 // users._nextCount()
 //     .then(count => console.log(`The next ID will be ${count}`));
 plugin.applyPlugin().catch(e => {
-        // Plugin failed to initialise
-        console.log("############### init failed: " + e);
-    });
+    // Plugin failed to initialise
+    console.log("############### init failed: " + e);
+});
 
 
 module.exports = mongoose.model('1601c_forms', Form1601CSchema);
