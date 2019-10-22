3d04879e15c9ead9a33f7945f2effecc6497af55
rcbermudez <rbermudez@centralizedinc.com>
2019-10-22 10:08:57 +0800

    form1601fq

--- src/components/FormDisplay.vue
+++ src/components/FormDisplay.vue
@@ -25,7 +25,7 @@ import Form2000ot from "../plugins/pdf/printers/2000ot";
 import Form1600wp from "../plugins/pdf/printers/1600wp";
 import Form1604e from "../plugins/pdf/printers/1604e";
 import Form1601f from "../plugins/pdf/printers/1601f";
-import Form1601f from "../plugins/pdf/printers/1601fq";
+import Form1601fq from "../plugins/pdf/printers/1601fq";
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
@@ -40,7 +40,7 @@ const printers = {
   FORM2000OT: Form2000ot,
   FORM1600WP: Form1600wp,
   FORM1601F: Form1601f,
-  FORM1601F: Form1601fq,
+  FORM1601FQ: Form1601fq,
 };
 export default {
   props: ["form", "type"],
