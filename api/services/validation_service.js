874196219a312f2e2a977c14f012cd8644696ee7
unknown <ariel.a.balita@gmail.com>
2019-10-28 16:32:49 +0800

    Form 1603q Model, Dao, Services and Validator

--- api/services/validation_service.js
+++ api/services/validation_service.js
@@ -20,6 +20,7 @@ const form1702exValidator = require('../validators/form1702exValidator');
 const form1702rtValidator = require('../validators/form1702rtValidator');
 const form1701aValidator = require('../validators/form1701aValidator');
 const form1601eqValidator = require('../validators/form1601eqValidator');
+const form1603qValidator = require('../validators/form1603qValidator');
 
 /**
  * 
@@ -67,6 +68,10 @@ function validate(form_type, form_details) {
             return form1701aValidator.validate(form_details);
         case "1601EQ":
             return form1601eqValidator.validate(form_details);
+        case "1603Q":
+            return form1603qValidator.validate(form_details);
+        default:
+            return { errors: [{ error: "Invalid Form Validator" }], form_details }
     }
 }
 
