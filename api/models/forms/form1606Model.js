9572f1a94c3c5c9db2f7529830b2ef6eb0acba49
Kris Bernard Yango <krisbernardyango@Kriss-MacBook-Pro.local>
2019-10-30 09:37:08 +0800

    1606

--- api/models/forms/form1606Model.js
+++ api/models/forms/form1606Model.js
@@ -1,9 +1,9 @@
 /**
  * 
  * @description FORM 1606 (JULY 1999)
- * @author Venus
+ * @author Kris
  * @base_form https://www.lawphil.net/administ/bir/frms/pymnt/1606.pdf
- * @version 1.0 - 10/15/2019
+ * @version 1.1 - 10/30/2019
  * 
  */
 
@@ -13,119 +13,172 @@ const autoIncrement = require('mongoose-auto-increment-reworked').MongooseAutoIn
 const common_model = require('./commonModels');
 
 const model_schema = {
-    category_of_agent: {
-        type: String
-    },
-    atc_code: {
-        type: String
-    }, // WI155
-    taxes_withheld: {
+    taxes_withheld: { //Item 4
         type: Boolean
     },
-    classification_property: {
+    //Part I
+    atc_code: { //Item 13
+        type: String
+    }, // WI155
+    category_of_agent: { //Item 14
         type: String
     },
-    location_property: {
+    classification_property: { //Item 15
+        type: String
+        /**
+         * 1 - Residential
+         * 2 - Agricultural
+         * 3 - Commercial
+         * 4 - Industrial
+         * 5 - Condominium Residential
+         * 6 - Condominium Commercial
+         * Others Specify 
+         */
+    },
+    classification_property_specify: { //Item 15
         type: String
     },
-    location_rdo_code: {
+    location_property: { //Item 16
         type: String
     },
-    tct_no: {
-        type: String
-    },
-    area_sold: {
-        type: String
-    },
-    selling_price_property: {
+    // location_rdo_code: { // ??????????????
+    //     type: String
+    // },
+    brief_descriptio: { //Item 17
+        tct_no: { 
+            type: String
+        },
+        area_sold: {
+            type: String
+        },
+        tax_dec_no: {
+            type: String
+        },
+        others:{
+            type: String
+        }
+    },  
+    selling_price_property: { //Item 18
         type: Boolean,
         default: false
     },
-    description_transaction: {
+    description_transaction: { //Item 20
+        type: String
+        /**
+         * 1 - Cash Sales
+         * 2 - Excempt
+         * 3 - Installment Sale
+         * 4 - Foreclosure Sale
+         * 5 - Others
+         * If excempt or Others please specify
+         */
+    },
+    description_transaction_specify:{ //Item 20 specify
         type: String
     },
-    selling_price: { type: Number, default: 0 },
-    cost_expenses: { type: Number, default: 0 },
-    mortgage_assumed: { type: Number, default: 0 },
-    total_payments_initial_year: { type: Number, default: 0 },
-    amount_installment_month: { type: Number, default: 0 },
-    tax_declaration_land: { type: Number, default: 0 },
-    tax_declaration_improvements: { type: Number, default: 0 },
-    zonal_value: { type: Number, default: 0 },
-    commissioner: { type: Number, default: 0 },
-    gross_selling_price: { type: Number, default: 0 },
-    fair_market_value: { type: Number, default: 0 }, // Sum of 27A & 27B/ 27C & 27D/ 27B & 27C/27A & 27D whichever is higher
-    bid_price: { type: Number, default: 0 },
-    installment_collected: { type: Number, default: 0 }, //Installment Sale excluding interest
-    taxable_base_computation: { type: Number, default: 0 },
-    particular_cash: [{
-        drawee_bank: {
-            type: Number,
-            default: 0
-        },
-        number: {
-            type: Number,
-            default: 0
-        },
-        date: {
-            type: Date
-        },
-        amount: {
-            type: Number,
-            default: 0
-        }
-    }],
-    particular_check: [{
-        drawee_bank: {
-            type: Number,
-            default: 0
-        },
-        number: {
-            type: Number,
-            default: 0
-        },
-        date: {
-            type: Date
-        },
-        amount: {
-            type: Number,
-            default: 0
-        }
-    }],
-    particular_tax_debit: [{
-        drawee_bank: {
-            type: Number,
-            default: 0
-        },
-        number: {
-            type: Number,
-            default: 0
-        },
-        date: {
-            type: Date
-        },
-        amount: {
-            type: Number,
-            default: 0
-        }
-    }],
-    particular_others: [{
-        drawee_bank: {
-            type: Number,
-            default: 0
-        },
-        number: {
-            type: Number,
-            default: 0
-        },
-        date: {
-            type: Date
-        },
-        amount: {
-            type: Number,
-            default: 0
-        }
-    }],
+    selling_price: { type: Number, default: 0 }, //Item 21
+    cost_expenses: { type: Number, default: 0 }, //Item 22
+    mortgage_assumed: { type: Number, default: 0 }, //Item 23
+    total_payments_initial_year: { type: Number, default: 0 }, //Item 24
+    amount_installment_month: { type: Number, default: 0 }, //Item 25
+    total_no_installment_month:{ type: Number, default: 0 }, //Item 26
+
+    //Item 27 Fair Market Value (FMV)
+    item27:{
+        tax_declaration_land: { type: Number, default: 0 }, //Item 27A
+        tax_declaration_improvements: { type: Number, default: 0 },// Item 27B
+        zonal_value: { type: Number, default: 0 }, //Item 27C
+        commissioner: { type: Number, default: 0 }, //Item 27D
+    },
+    //Item 28 Determination of Taxable Base
+    item28:{
+        gross_selling_price: { type: Number, default: 0 }, //Item 28A
+        fair_market_value: { type: Number, default: 0 }, //Item 28B Sum of 27A & 27B/ 27C & 27D/ 27B & 27C/27A & 27D whichever is higher
+        bid_price: { type: Number, default: 0 }, //Item 28C
+        installment_collected: { type: Number, default: 0 }, //Item 28D //Installment Sale excluding interest
+        others:{ type: String }, // Item 28E
+        computation:{ type: String},
+        computation2:{ type: String}
+    },
+
+    seller_hab_eng:{type: Boolean}, //Item 29
+    tax_base:{ type: Number, default: 0 }, //Item 30
+    tax_rate:{ type: Number, default: 0 }, //Item 31
+    tax_req_withheld:{ type: Number, default: 0 }, //Item 32
+    less_tax_paid:{ type: Number, default: 0 }, //Item 33
+    tax_due:{ type: Number, default: 0 }, //Item 34
+
+
+    //Part III
+    // taxable_base_computation: { type: Number, default: 0 }, //Item 30
+    // particular_cash: [{
+    //     drawee_bank: {
+    //         type: Number,
+    //         default: 0
+    //     },
+    //     number: {
+    //         type: Number,
+    //         default: 0
+    //     },
+    //     date: {
+    //         type: Date
+    //     },
+    //     amount: {
+    //         type: Number,
+    //         default: 0
+    //     }
+    // }],
+    // particular_check: [{
+    //     drawee_bank: {
+    //         type: Number,
+    //         default: 0
+    //     },
+    //     number: {
+    //         type: Number,
+    //         default: 0
+    //     },
+    //     date: {
+    //         type: Date
+    //     },
+    //     amount: {
+    //         type: Number,
+    //         default: 0
+    //     }
+    // }],
+    // particular_tax_debit: [{
+    //     drawee_bank: {
+    //         type: Number,
+    //         default: 0
+    //     },
+    //     number: {
+    //         type: Number,
+    //         default: 0
+    //     },
+    //     date: {
+    //         type: Date
+    //     },
+    //     amount: {
+    //         type: Number,
+    //         default: 0
+    //     }
+    // }],
+    // particular_others: [{
+    //     drawee_bank: {
+    //         type: Number,
+    //         default: 0
+    //     },
+    //     number: {
+    //         type: Number,
+    //         default: 0
+    //     },
+    //     date: {
+    //         type: Date
+    //     },
+    //     amount: {
+    //         type: Number,
+    //         default: 0
+    //     }
+    // }],
 };
 
 var Form1606Schema = new Schema({...common_model, ...model_schema });
