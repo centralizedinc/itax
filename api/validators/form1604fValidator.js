345e0fee74d1b3faa1f92e2063dc3130e475895a
VPBelo <venus.belo.rhi@gmail.com>
2019-11-04 16:35:51 +0800

    form1604fvalidator

--- api/validators/form1604fValidator.js
+++ api/validators/form1604fValidator.js
@@ -7,40 +7,35 @@ function validate(form_details) {
     //validation begins ...
     var errors = [];
 
-    //validate taxpayer
+    //validate required fields
     errors.push(...commonValidator.validateTaxpayerDetails(form_details.taxpayer, 1));
     console.log('done validating taxpayer...');
 
     // validate required fields
     errors.push(...validateRequired(form_details));
-    console.log('done required fields...');
+    console.log('done validating required fields...');
 
-    // check due date if late filing
-    var {
-        error_messages,
-        form_details
-    } = commonValidator.checkDueDate(form_details, 2);
+    // checking due date if late filing
+    var { error_messages, form_details } = commonValidator.checkDueDate(form_details, 2);
     errors.push(...error_messages);
     console.log('done checking due date...');
 
-    return {
-        errors,
-        form_details
-    }
+    return { errors, form_details }
 }
 
 function validateRequired(form) {
     var error_messages = [];
-    console.log('Validating required fields...');
+    console.log('Validating required fields ...');
+   
 
     if (!form.category_of_agent) {
-        error_messages.push({
-            page: 1,
-            field: "category_of_agent",
-            error: constant_helper.MANDATORY_FIELD('Category of Agent')
-        });
+        error_messages.push({ page: 1, field: "category_of_agent", error: constant_helper.MANDATORY_FIELD('Category of Withholding Agent') });
     }
-
+    if (form.is_avail_tax_relief) {
+        
+    } else if(!form.avail_tax_relief){
+        error_messages.push({ page: 1, field: "avail_tax_relief", error: "Please specify" });
+    }
     return error_messages;
 }
 
